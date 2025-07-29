8dbf14cb71f2b18827eee8b6478cda2fc62cd6b1
Chirag Dhamange <c.d.marshall1869@gmail.com>
2025-07-28 18:15:48 -0700

    upload documents api created

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,24 +1,49 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, BackgroundTasks
+from fastapi.responses import FileResponse, JSONResponse
 from typing import List
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+import sys
+import uuid
+
+CURRENT_DIR = Path(__file__).resolve().parent
+SERVICES_DIR = (CURRENT_DIR / "../services").resolve()
+
+sys.path.append(str(SERVICES_DIR))
+
+from generate_filled_1040 import generate_filled_1040
 
 router = APIRouter()
+
+OUTPUT_DIR = (CURRENT_DIR / "../static/generated_documents/").resolve()
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 @router.get("/")
 async def home():
   return { "message" : "AI Tax Return Agent backend is running" }
 
-# Upload files, extract data, calculate taxes, and return Form 1040
-@router.post("/extract-and-calculate")
-async def extract_and_calculate(files : List[UploadFile] = File(...)):
-  results = []
-
+# Upload files, extract data, calculate taxes, and generate Form 1040
+@router.post("/upload_documents", response_class = FileResponse)
+async def upload_documents(
+  background_tasks: BackgroundTasks,
+  files : List[UploadFile] = File(...)
+):
+  temp_pdf_paths = []
   for file in files:
-    file_name = file.filename
     content = await file.read()
+    with NamedTemporaryFile(suffix=".pdf", delete=False) as tmp:
+      tmp.write(content)
+      temp_path = Path(tmp.name)
+      temp_pdf_paths.append(temp_path)
+      background_tasks.add_task(temp_path.unlink, missing_ok=True)
 
-    # TODO: logic for redaction, extraction, calculation
+  input_pdf_path = (
+    CURRENT_DIR / "../static/templates/f1040_2024.pdf"
+  ).resolve(strict=True)
 
-    results.append({ "file_name" : file_name, "size" : len(content) })
+  document_id = f"{uuid.uuid4().hex}.pdf"
+  output_pdf_path = OUTPUT_DIR / document_id
 
-  # TODO: Return a pdf
-  return { "status" : "ok", "uploaded" : results }
+  generate_filled_1040(temp_pdf_paths, input_pdf_path, output_pdf_path)
+
+  return JSONResponse(content = { "document_id": document_id })
