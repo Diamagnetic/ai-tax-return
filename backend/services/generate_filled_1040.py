e3dc1e24360aeaa8ad7a246ecbf134d7ea09b6bf
Chirag Dhamange <c.d.marshall1869@gmail.com>
2025-07-28 17:17:51 -0700

    updated imports

--- backend/services/generate_filled_1040.py
+++ backend/services/generate_filled_1040.py
@@ -9,6 +9,7 @@ TAX_POLICY_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "tax_policy"))
 if TAX_POLICY_DIR not in sys.path:
     sys.path.insert(0, TAX_POLICY_DIR)
 
+from form_extractor import FormExtractor
 from tax_calculator import TaxFormData
 from tax_policy_config import SingleFiler2024Config
 
