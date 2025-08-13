66cb207b5d92ac3f4985bcc7bd50c6be5f9c1654
Chirag Dhamange <c.d.marshall1869@gmail.com>
2025-08-12 21:14:58 -0700

    fixed endpoints names and values

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,4 @@
 # Server configuration
 SERVER_URL = "https://ai-tax-return-backend-92e5b1232028.herokuapp.com"
-UPLOAD_ENDPOINT = f"{SERVER_URL}/upload_documents"
+SUBMIT_ENDPOINT = f"{SERVER_URL}/submit_tax_form"
 GET_FORM_ENDPOINT = f"{SERVER_URL}/documents"
