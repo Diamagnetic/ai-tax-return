266f58ebbe0480e04f5a9a001317a41dbd23e665
Chirag Dhamange <c.d.marshall1869@gmail.com>
2025-09-16 02:10:52 -0700

    updated frontend for nginx configuration

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,4 @@
 # Server configuration
-SERVER_URL = "https://ai-tax-return-backend-92e5b1232028.herokuapp.com"
-SUBMIT_ENDPOINT = f"{SERVER_URL}/submit_tax_form"
-GET_FORM_ENDPOINT = f"{SERVER_URL}/documents"
+SERVER_URL = "http://localhost:8000"
+SUBMIT_ENDPOINT = f"{SERVER_URL}/api/submit_tax_form"
+GET_FORM_ENDPOINT = f"{SERVER_URL}/api/documents"
